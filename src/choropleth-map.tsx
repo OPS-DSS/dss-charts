0860119e47c7c851781b3956af5757922e91ab5e
cchavezocla <cchavezo@gmail.com>
2026-09-14 18:27:04 -0300

    Usar Stadia Stamen Toner Background como basemap del choropleth (#8)
    * Use Esri World Physical basemap
    
    * Use Stadia Stamen Toner Background basemap

--- src/choropleth-map.tsx
+++ src/choropleth-map.tsx
@@ -98,13 +98,10 @@ export const DSChoroplethMap = ({
 
     const map = L.map(mapRef.current).setView(center, zoom)
 
-    L.tileLayer(
-      'https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png',
-      {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-      },
-    ).addTo(map)
+    L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner_background/{z}/{x}/{y}{r}.png', {
+      attribution:
+        '&copy; Stadia Maps &copy; Stamen Design &copy; OpenMapTiles &copy; OpenStreetMap contributors',
+    }).addTo(map)
 
     mapInstanceRef.current = map
 
