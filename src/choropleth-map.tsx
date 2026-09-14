4228f9aafa7c5bccde74368429c4e39cb92ed0d0
cchavezocla <cchavezo@gmail.com>
2026-09-14 18:14:20 -0300

    Switch choropleth basemap to OpenStreetMap

--- src/choropleth-map.tsx
+++ src/choropleth-map.tsx
@@ -98,13 +98,10 @@ export const DSChoroplethMap = ({
 
     const map = L.map(mapRef.current).setView(center, zoom)
 
-    L.tileLayer(
-      'https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png',
-      {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-      },
-    ).addTo(map)
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map)
 
     mapInstanceRef.current = map
 
