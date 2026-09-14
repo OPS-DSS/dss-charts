c7253a2391bc341eacd966107f3645556a12ea8c
cchavezocla <cchavezo@gmail.com>
2026-09-14 18:14:28 -0300

    Update built choropleth basemap

--- dist/chunk-PUUTAKVJ.mjs
+++ dist/chunk-PUUTAKVJ.mjs
@@ -27,12 +27,9 @@ var DSChoroplethMap = ({
   useEffect(() => {
     if (!mapRef.current || mapInstanceRef.current) return;
     const map = L.map(mapRef.current).setView(center, zoom);
-    L.tileLayer(
-      "https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png",
-      {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-      }
-    ).addTo(map);
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
     mapInstanceRef.current = map;
     return () => {
       if (mapInstanceRef.current) {
